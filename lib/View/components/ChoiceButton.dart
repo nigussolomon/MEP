1e5242cee22833262f3fb69274758e83d0610681
nonyx777 <nahomretebo@gmail.com>
2023-06-29 22:00:48 +0300

    choice button component implemented

---
+++ lib/View/components/ChoiceButton.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class ChoiceButton extends StatelessWidget {
+  final String choiceContent;
+  const ChoiceButton({
+    required this.choiceContent,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: EdgeInsets.all(height * 0.02),
+      child: Container(
+        padding: EdgeInsets.all(height * 0.023),
+        height: height * 0.077,
+        width: width * 0.15,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 72, 72, 72),
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: Center(
+          child: Text(
+            choiceContent,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: height * 0.02,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
