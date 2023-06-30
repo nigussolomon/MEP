72d522db7dccb2851223898b03c88fc6577779ca
nonyx777 <nahomretebo@gmail.com>
2023-06-30 13:48:39 +0300

    SkipButton component implemented(decoupled)

--- lib/View/Questions/QuestionsPage.dart
+++ lib/View/Questions/QuestionsPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mep/Bloc/question/question_bloc.dart';
 import 'package:mep/View/components/QuestionTile.dart';
+import 'package:mep/View/components/SkipButton.dart';
 
 class QuestionPage extends StatefulWidget {
   const QuestionPage({super.key});
@@ -22,7 +23,6 @@ class _QuestionPageState extends State<QuestionPage> {
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
-    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: BlocBuilder<QuestionBloc, QuestionState>(
         builder: (context, state) {
@@ -70,37 +70,7 @@ class _QuestionPageState extends State<QuestionPage> {
                     ],
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.all(height * 0.05),
-                  child: Container(
-                    padding: EdgeInsets.all(height * 0.023),
-                    height: height * 0.077,
-                    width: width * 0.15,
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 72, 72, 72),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Skip Question",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: height * 0.02,
-                          ),
-                        ),
-                        Icon(
-                          Icons.arrow_right_alt_outlined,
-                          color: Colors.white,
-                          size: height * 0.04,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+                const SkipButton(),
               ],
             );
           }
