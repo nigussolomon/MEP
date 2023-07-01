58296dbb11321d579de5fecbb9192a5530ed6abf
nonyx777 <nahomretebo@gmail.com>
2023-07-01 19:06:14 +0300

    hovering effect for the buttons

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 72, 72, 72)),
           useMaterial3: true,
         ),
         debugShowCheckedModeBanner: false,
