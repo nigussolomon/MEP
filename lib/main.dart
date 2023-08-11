b25d19554ed8fc780c352c9fee0820165d61ea0d
nigussolomon <nigussolomon444@gmail.com>
2023-08-11 19:56:02 +0300

    added a setup screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mep/View/IntroPages/login.dart';
+import 'package:mep/View/IntroPages/Setup.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'AbyssinicaSIL-Regular',
       ),
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: SetupPage(),
     );
   }
 }
